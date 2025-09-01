584fce67c548fadbbd91afe0480b1a589ce0c3b6
Riya <riiyaraj079@gmail.com>
2025-09-02 00:59:38 +0530

    Time: 70 ms (55.06%), Space: 171.9 MB (48.26%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> mpp;
-        for (string w : words) mpp[w]++;
+        unordered_map<string, int> mp;
 
-        int count = 0, alreadyPalindrome = 0;
-        for (auto& [w, freq] : mpp) {
-            string s = w;
-            reverse(s.begin(), s.end());
-            if (w == s) {
-                count += (freq / 2) * 4;
-                if (freq % 2) alreadyPalindrome = 1;
-            } else if (w < s && mpp.count(s)) {
-                count += min(freq, mpp[s]) * 4;
+        int result = 0;
+
+        for(string &word : words){
+            string reverseWord = word;
+            swap(reverseWord[0], reverseWord[1]);
+
+            if(mp[reverseWord] > 0) {
+                result += 4;
+                mp[reverseWord]--;
+            }else{
+                mp[word]++;
             }
         }
-        return count + alreadyPalindrome * 2;
+        
+        //check equal character word, use only one
+        for(auto &it : mp){
+            string word = it.first;
+            int count = it.second;
+
+            if(word[0] == word[1] && count > 0) {
+                result += 2;
+                break;
+            }
+        }
+        return result;
     }
 };
