729ac90fb65eee5cfb8c530429537fcd3adf312f
Riya <riiyaraj079@gmail.com>
2025-09-03 17:05:39 +0530

    Time: 7 ms (97.29%), Space: 47.3 MB (42.12%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,44 @@
+class Solution { 
+public:     
+    vector<int> getNSL(vector<int>& arr, int n) {
+        vector<int> result(n);
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            result[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return result;
+    }
+    
+    vector<int> getNSR(vector<int>& arr, int n) {
+        vector<int> result(n);
+        stack<int> st;
+        for(int i = n-1; i >= 0; i--) {
+            while(!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            result[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return result;
+    }
+       
+    int sumSubarrayMins(vector<int>& arr){         
+        int n = arr.size();               
+        vector<int> NSL = getNSL(arr, n);         
+        vector<int> NSR = getNSR(arr, n);          
+        long long sum =0;         
+        int M = 1e9+7;                
+        for(int i = 0; i<n; i++){             
+            long long ls = i-NSL[i]; //left me kitne elements honge             
+            long long rs = NSR[i]-i; //right me kitne elements honge              
+            long long totalWays = ls*rs;              
+            long long totalSum = (1LL * arr[i] * totalWays) % M;              
+            sum = (sum + totalSum)%M;          
+        }                  
+        return sum;     
+    } 
+};
