81871d7b6e2c4492a9a67feb3f05374a2fd50b0a
Riya <197113595+riyaraj-web@users.noreply.github.com>
2025-09-07 01:43:27 +0530

    Time: 3 ms (79%), Space: 14.7 MB (57.82%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy -> next = head;
+        ListNode *pre = dummy, *curr = head;
+        while (curr) {
+            if ((curr -> next) && (curr -> next -> val < curr -> val)) {
+                while ((pre -> next) && (pre -> next -> val < curr -> next -> val)){
+                    pre = pre -> next;
+                }
+                ListNode* temp = pre -> next;
+                pre -> next = curr -> next;
+                curr -> next = curr -> next -> next;
+                pre -> next -> next = temp;
+                pre = dummy;
+            }
+            else{
+                curr = curr -> next;
+            }
+        }
+        return dummy -> next;
+    }
+};
