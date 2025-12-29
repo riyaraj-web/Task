f444647f0dfc78f69d1812ac3d79c2f5332b7ab2
Riya <197113595+riyaraj-web@users.noreply.github.com>
2025-12-30 01:41:00 +0530

    Time: 18 ms (10.47%), Space: 14.6 MB (48.47%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    std::string minRemoveToMakeValid(std::string s) {
-        int leftCount = 0;
-        int rightCount = 0;
-        std::stack<char> stack;
+    string minRemoveToMakeValid(string s) {
+        int n = s.length();
 
-        //pass 1
-        for (char ch : s){
-            if (ch == '('){
-                leftCount++;
-            } else if (ch == ')'){
-                rightCount++;
-            }
-            if (rightCount > leftCount){
-                rightCount--;
-                continue;
-            } else {
-                stack.push(ch);
+        stack<int> st;
+        unordered_set<int> remove_idx;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                st.push(i);
+            } else if(s[i] == ')') {
+                if(st.empty()) {
+                    remove_idx.insert(i);
+                } else {
+                    st.pop();
+                }
             }
         }
-
-        std::string result = "";
         
-        // Pass 2
-        while (!stack.empty()) {
-            char currentChar = stack.top();
-            stack.pop();
-            if (leftCount > rightCount && currentChar == '(') {
-                leftCount--;
-            } else {
-                result += currentChar;
-            }
+        //only open brackets "((("
+        while(!st.empty()) {
+            remove_idx.insert(st.top());
+            st.pop();
         }
 
-        // Reverse the result string
-        std::reverse(result.begin(), result.end());
-        return result;
+        string result = "";
 
+        for(int i = 0; i < n; i++){
+            if(remove_idx.find(i) == remove_idx.end()) {
+                result.push_back(s[i]);
+            }
+        }
+        return result;
     }
 };
