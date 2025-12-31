a6d7cb0f4722f4ac4c27a0dd57c304a43cbac940
Riya <197113595+riyaraj-web@users.noreply.github.com>
2025-12-31 17:07:46 +0530

    Time: 12 ms (22.76%), Space: 45.2 MB (85.25%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -1,35 +1,27 @@
- class Solution {
+class Solution {
 public:
-    ListNode *reverseLL(ListNode * head){
-        if(!head) return head;
-        ListNode *temp,*prev=NULL;
-        while(head){                                            // reversing the linked list
-            temp=head->next;
-            head->next=prev;
-            prev=head;
-            head=temp;
-        }
-        return prev;
-    }
-    vector<int> nextLargerNodes(ListNode* head) {
-        vector<int> result;
-        stack<int> st;
-        head=reverseLL(head);
-        while(head){                                            // traversing the LL from backwards(as we reversed the LL)
-            if(st.empty()){                                     
-                result.push_back(0);
-                st.push(head->val);
+        int count(ListNode *head){
+            int res=0;
+            while(head){
+                res++;
                 head=head->next;
             }
-            else if(st.top()>head->val){
-                result.push_back(st.top());
-                st.push(head->val);
-                head=head->next;    
-            }
-            else     st.pop();
+            return res;
         }
-     // reversing the result vector for getting the resultant output (as we had reversed the LL then found out the greater node to it)
-        reverse(result.begin(),result.end());                  
-        return result;
+        vector<int> nextLargerNodes(ListNode* head) {
+            int n=count(head);
+            vector<int> ans(n,0);
+            stack<pair<int,int>> s;
+            int i=0;
+            while(head){
+                while(!s.empty() && s.top().first<head->val){
+                    int idx=s.top().second;
+                    s.pop();
+                    ans [idx] = head->val;
+                }
+                s.push({head->val,i++});
+                head=head->next;
+        }
+        return ans;
     }
 };
