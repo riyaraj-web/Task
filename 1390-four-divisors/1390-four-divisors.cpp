b9d4e41f43d83d6a7ded3a9c72da1149277a336e
Riya <197113595+riyaraj-web@users.noreply.github.com>
2026-01-12 23:57:17 +0530

    Time: 8 ms (71.77%), Space: 16 MB (94.14%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int findSumDivisors(int num) {
+    int findSumDivisors(int num){
         int divisors = 0; //count
-        int sum = 0; //sum of divisors
+        int sum = 0;   //sum of divisors
 
-        for (int fact = 1; fact*fact <= num; fact++) {
-            if(num % fact == 0) {
-                int other =  num/fact;
+        for(int fact = 1; fact*fact <= num; fact++){
+            if(num % fact == 0){
+                int other = num/fact;
 
-                if(other == fact) { //perfect square number ka cash hai ye
+                if(other == fact) { //perfect square number
                     divisors += 1;
                     sum += fact;
                 } else {
@@ -16,20 +16,19 @@ public:
                     sum += (fact + other);
                 }
             }
+
             if(divisors > 4)
                 return 0; //invalid
         }
-
         return divisors == 4 ? sum : 0;
     }
-        
-    int sumFourDivisors(vector<int>& nums){
+    int sumFourDivisors(vector<int>& nums) {
         int result = 0;
 
-        for (int &num : nums) {
+        for(int &num : nums){
             result += findSumDivisors(num);
         }
-
+         
         return result;
     }
 };
