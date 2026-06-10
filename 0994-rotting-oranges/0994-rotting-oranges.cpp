bc39e7ebc214f0e8a39faf0510c85665e186bb0e
Riya <197113595+riyaraj-web@users.noreply.github.com>
2026-06-11 03:04:29 +0530

    Time: 0 ms (100%), Space: 17.2 MB (28.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,11 +4,11 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> visited = grid;
-        //making queue in which we will fill rotten oranges
+
         queue<pair<int, int>> q;
         int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j <n; j++){
                 if (visited[i][j] == 2) {
                     q.push({i, j});
                 }
@@ -17,24 +17,23 @@ public:
                 }
             }
         }
-        //q.empty() means there is no rotten orange in the grid and countFreshOrange = 0 means we will rotten the freshoranges in 0 mins
         if (countFreshOrange == 0)
             return 0;
         if (q.empty())
-            return -1;
-        
+           return -1;
+
         int minutes = -1;
-        // we will cover four directions i.e. up, down, left, right
+
         vector<pair<int, int>> dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
         while (!q.empty()) {
             int size = q.size();
             while (size--) {
-                auto [x, y] = q.front();
+                auto [x,y] = q.front();
                 q.pop();
                 for (auto [dx, dy] : dirs) {
                     int i = x + dx;
                     int j = y + dy;
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
+                    if (i >= 0 && i < m && j >= 0 && j < n  && visited[i][j] == 1){
                         visited[i][j] = 2;
                         countFreshOrange--;
                         q.push({i, j});
@@ -43,9 +42,9 @@ public:
             }
             minutes++;
         }
-        
+
         if (countFreshOrange == 0)
-            return minutes;
+             return minutes;
         return -1;
     }
 };
